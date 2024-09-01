99eb72484a7866e36feabfa3b96daa5e151cf5b1
Fabrizio Gasparini <makerfaffa@gmail.com>
2024-09-01 15:35:50 +0200

    Update token.js

--- js/token.js
+++ js/token.js
@@ -1,4 +1,4 @@
-url = "https://accounts.google.com/o/oauth2/auth?client_id=14536006715-6cm2ajnipa0qr8v1s5ot1h2e5jrmg14s.apps.googleusercontent.com&redirect_uri=https://fabriziogasparini.github.io/SpikeBot/redirect.html&response_type=code&scope=openid%20email%20profile"
+url = "https://accounts.google.com/o/oauth2/auth?client_id=14536006715-6cm2ajnipa0qr8v1s5ot1h2e5jrmg14s.apps.googleusercontent.com&redirect_uri=https://fabriziogasparini.github.io/spikebot/redirect.html&response_type=code&scope=openid%20email%20profile"
 
 window.addEventListener('load', () => {
     window.open(url, "_blank")
